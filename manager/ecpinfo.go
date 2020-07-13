5af73bed424e4662aa618f431209978d96a95171
jiaozhengda <jiaozhengda@chinamobile.com>
2020-07-13 07:01:10 -0700

    add ecp info

---
+++ manager/ecpinfo.go
@@ -0,0 +1,10 @@
+/*
+@Time : 20-7-13
+@Author : jzd
+@Project: apigw
+*/
+package manager
+
+//ecp interface define all ecp CRUDs
+type EcpManager interface {
+}
--- model/ecpinfo.go
+++ model/ecpinfo.go
@@ -6,4 +6,10 @@
 package model
 
 type EcpInfo struct {
+	ID     string `orm:"pk;column(id)" json:"id"`
+	Name   string `orm:"column(name)" json:"name"`
+	Region string `orm:"column(region)" json:"region"`
+	Ip     string `orm:"column(ip)" json:"ip"`
+	Port   string `orm:"column(port)" json:"port"`
+	Info   string `orm:"column(port)" json:"port"`
 }
